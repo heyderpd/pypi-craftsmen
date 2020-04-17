3808f6f670db837f63261a9da265ced6c43b621a
heyder.dias <heyder.dias@zoop.com.br>
2020-04-17 19:34:11 -0300

    add basic setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Pytils',
+  packages = ['Pytils'],
+  version = '0.1',
+  license='MIT',
+  description = 'pytils description',
+  author = 'heyderpd',
+  author_email = 'heyderpd@gmail.com',
+  url = 'https://github.com/heyderpd/pypi-pytils',
+  download_url = 'https://github.com/heyderpd/pypi-pytils/archive/v_01.tar.gz',
+  keywords = ['ramda', 'utils', 'functional'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
