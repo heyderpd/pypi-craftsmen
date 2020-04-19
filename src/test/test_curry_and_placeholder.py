f0cf3e794d1cf6088ebb6e911e825b24fdde7a5b
heyder.dias <heyder.dias@zoop.com.br>
2020-04-19 15:02:33 -0300

    create test for curry with placeholder

--- src/test/test_curry_and_placeholder.py
+++ src/test/test_curry_and_placeholder.py
@@ -1,20 +1,7 @@
-from src.craftsmen.curry_with_placeholder import curry
+from src.craftsmen import curry, p
 from src.test import create_dummie_mock, mock_result, arg1, arg2, arg3
 
-def test_mock_function():
-    _dummie, _mock = create_dummie_mock()
-    result = _dummie(arg1, arg2, arg3)
-    assert result is mock_result
-    _mock.assert_called_once_with(arg1, arg2, arg3)
-
-def test_curry_natural_use():
-    _dummie, _mock = create_dummie_mock()
-    curry_function = curry(_dummie)
-    result = curry_function(arg1, arg2, arg3)
-    assert result is mock_result
-    _mock.assert_called_once_with(arg1, arg2, arg3)
-
-def test_curry_sequential_use():
+def test_curry_with_none_placeholder():
     _dummie, _mock = create_dummie_mock()
     curry_function = curry(_dummie)
     partial = curry_function(arg1)
@@ -23,10 +10,47 @@ def test_curry_sequential_use():
     assert result is mock_result
     _mock.assert_called_once_with(arg1, arg2, arg3)
 
-def test_curry_partial_sequential_use():
+def test_curry_with_one_placeholder():
     _dummie, _mock = create_dummie_mock()
     curry_function = curry(_dummie)
-    partial = curry_function(arg1)
-    result = partial(arg2, arg3)
+    partial = curry_function(p, p, arg3)
+    partial = partial(arg1)
+    result = partial(arg2)
+    assert result is mock_result
+    _mock.assert_called_once_with(arg1, arg2, arg3)
+
+def test_curry_with_two_placeholder():
+    _dummie, _mock = create_dummie_mock()
+    curry_function = curry(_dummie)
+    partial = curry_function(p, arg2, arg3)
+    result = partial(arg1)
+    assert result is mock_result
+    _mock.assert_called_once_with(arg1, arg2, arg3)
+
+def test_curry_with_midle_placeholder():
+    _dummie, _mock = create_dummie_mock()
+    curry_function = curry(_dummie)
+    partial = curry_function(p, arg2, p)
+    partial = partial(arg1)
+    result = partial(arg3)
+    assert result is mock_result
+    _mock.assert_called_once_with(arg1, arg2, arg3)
+
+def test_curry_with_partial_placeholder():
+    _dummie, _mock = create_dummie_mock()
+    curry_function = curry(_dummie)
+    partial = curry_function(p, arg2)
+    partial = partial(arg1)
+    result = partial(arg3)
+    assert result is mock_result
+    _mock.assert_called_once_with(arg1, arg2, arg3)
+
+def test_curry_with_full_placeholder():
+    _dummie, _mock = create_dummie_mock()
+    curry_function = curry(_dummie)
+    partial = curry_function(p, p, p)
+    partial = partial(arg1)
+    partial = partial(arg2)
+    result = partial(arg3)
     assert result is mock_result
     _mock.assert_called_once_with(arg1, arg2, arg3)
